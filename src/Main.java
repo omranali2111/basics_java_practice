a3c32d3bc4327e22d7fed75028e845d2b3ac1255
omranali2111 <aljabri1234@hotmail.com>
2023-12-10 15:15:46 +0400

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ArrayList<String> fruits = new ArrayList<String>();
+
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Date");
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
     }
 }
